2489e0940aa19a20f7f54ac63cfd9dfc1e72b09f
Eelke van den Bos <eelkevdbos@gmail.com>
2018-11-07 20:37:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ticket_universe',
       version='0.1',
       description='Toolset to generate ticket universes',
-      url='http://github.com/moddix/ticket_universe',
+      url='http://github.com/lotify/ticket_universe',
       author='Eelke van den Bos',
       author_email='eelke@moddix.com',
       license='MIT',
