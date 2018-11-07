5ada2140fcd9836ae02815875da5c2debe9a012d
Eelke van den Bos <eelkevdbos@gmail.com>
2018-11-07 21:24:30 +0100

    Add top level module initialization

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 clean:
-	rm -rf ./{build, dist}
+	rm -rf ./build ./dist
 
 build: clean
 	python3 setup.py sdist bdist_wheel
+
+upload: build
+	twine upload dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ticket_universe',
-    version='0.1.2',
+    version='0.1.3',
     description='Toolset to generate ticket universes',
     long_description=long_description,
     long_description_content_type="text/markdown",
